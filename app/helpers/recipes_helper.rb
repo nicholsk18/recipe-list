9a98b3dd98e3266789b533a09c6b4cba912f1d2c
Karson Nichols <nicholsk18@gmail.com>
2021-09-04 22:38:55 -0400

    update img for recipe

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,12 @@
 module RecipesHelper
+    def get_image(id)
+        image_file = Rails.root.join('public', 'uploads', "recipe-#{id}.png")
+
+        if !File.exist?(image_file)
+            # path to generic img
+            "/uploads/recipe-#{id}.png"
+        end
+
+        "/uploads/recipe-#{id}.png"
+    end
 end
